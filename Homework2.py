3ba42fc0e7117d0e3247644edcce71fc4da3bdd3
fedka1979 <fedka-3@yandex.ru>
2024-06-27 16:24:22 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,4 @@
+problem_count = 12
+time = 1.5
+name = 'Pyton'
+print('Курс' ,':', name ,',' ,'всего задач' ,':' ,problem_count ,',' , 'затрачено часов' ,':' ,time , 'среднее время выполнения' ,time/problem_count,'часа','.')
